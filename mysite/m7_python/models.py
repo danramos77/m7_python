28e240000fec4183e9b8d8c24c29beead0642bcf
Yerko Cruz <ycruzramos@gmail.com>
2024-08-10 15:28:07 -0400

    Proyecto con el hito 2

--- mysite/m7_python/models.py
+++ mysite/m7_python/models.py
@@ -28,17 +28,20 @@ class Inmuebles(models.Model):
     id_user = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=True)
     id_tipo_inmueble = models.ForeignKey('m7_python.Tipo_inmueble', on_delete=models.CASCADE, null=True)
     id_comuna = models.ForeignKey('m7_python.Comuna', on_delete=models.CASCADE, null=True)
-    id_region = models.ForeignKey('m7_python.Region', on_delete=models.CASCADE, null=True)
+    id_region = models.ForeignKey('m7_python.region', on_delete=models.CASCADE, null=True)
     nombre_inmueble = models.TextField()
     descripcion = models.CharField(max_length=200, blank=False, null=False)
-    m2_construidos = models.FloatField(default=0)
+    m2_construido = models.FloatField(default=0)
     numero_banos = models.IntegerField(default=0)
-    numero_hab = models.ImageField(default=0)
-    direccion = models.CharField(max_length=200)
+    numero_hab = models.IntegerField(default=0)
+    direccion = models.CharField(max_length=200) 
+    m2_terreno = models.FloatField(default=0)    #agregado
+    numero_est = models.IntegerField(default=0)   #agregado
+   
     #tipo_inmueble = models.CharField(max_length=10, null=False, blank=False)
     #precio_mensual = models.IntegerField(null=True, blank=True)
     #estado = models.BooleanField(null=False, blank=True )
-    #m2_terreno = models.FloatField(null=True, blank=True)
+   
     #numero_est = models.IntegerField(null=True, blank=True)
     #lat = models.FloatField(verbose_name='Latitud')
     #lng = models.FloatField(verbose_name='Longitud')
